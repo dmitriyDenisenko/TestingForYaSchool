95a2c0fe5828be65187d67189543e4b205001189
dmitriyDenisenko <dipster86@yandex.ru>
2022-09-14 17:32:13 +0300

    Generate: basic boot project

---
+++ src/main/java/ya/com/TestingForYaSchool/TestingForYaSchoolApplication.java
@@ -0,0 +1,13 @@
+package ya.com.TestingForYaSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingForYaSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingForYaSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ya/com/TestingForYaSchool/TestingForYaSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ya.com.TestingForYaSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingForYaSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
